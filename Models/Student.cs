26635533ef9447420b9c3accd2d6164298dfb7be
oksana.myronovych <Oksana.Myronovych@ndsu.edu>
2024-11-20 14:44:12 -0600

    New Class - Student.cs

---
+++ Models/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp8_nov20_cl2.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastDescription { get; set; }
+    }
+}
